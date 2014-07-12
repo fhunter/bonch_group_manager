e5f208a1e4b248366dce37545b146883c216953e
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2014-07-12 17:53:17 +0400

    Refactored group list showing

--- index.py
+++ index.py
@@ -6,14 +6,10 @@ import pwd
 import grp
 cgitb.enable()
 
-userbase=u"/selfreg/?getuser="
+userbase=u"/selfreg/?page=getuser&getuser="
 
 mainpage=u"""
 <h1>Добавление и просмотр групп</h1>
-<form method="get" action="./" name="groupsearch">
-Ключ фильтра:<input type="text" name="searchkey">
-<input type="submit" value="Submit">
-</form>
 <form method="post" action="" name="groupadd">
 Название группы:<input type="text" name="groupname">
 Количество студентов:<select name="number">
@@ -23,19 +19,29 @@ mainpage=u"""
 </select> 
 <input type="submit" value="Submit">
 </form>
-<a href="./?listadd=html">Очередь добавления групп</a><br>
+<a href="./?page=listadd">Очередь добавления групп</a><br>
 %s
 
 """
 
+showgrouppage=u"""
+<h1>Группа %s</h1>
+<table border=1>
+%s
+</table>
+<a href="./?page=mainpage">На главную</a>
+"""
+
 queuepage=u"""
 <h1>Очередь добавления групп</h1>
-<a href="./">На главную</a>
+<a href="./?page=mainpage">На главную</a>
 """
 
 errorpage=u"""
 <h1>Error</h1>
 %s
+<br>
+<a href="./?page=mainpage">На главную</a>
 """
 
 def header_html():
@@ -51,32 +57,73 @@ def print_ui(page):
 	</body></html>
 	"""
 
+def listadd_ui(form):
+	pass
+
+def showgroup_ui(form):
+	if "group" in form:
+		group=form["group"].value
+		try:
+			grp.getgrnam(group)
+		except:
+			print_ui(errorpage % ("No such group 1"))
+			return 
+		grpvalue = grp.getgrnam(group)
+		if ((grpvalue[2] < 1000) or (grpvalue[2] > 64000)):
+			print_ui(errorpage % ("No such group 2"))
+			return 
+		table= u"<tr><td>Группа</td><td>Пользователи</td><td>Комментарий к группе</td></tr>"
+		table += u"<tr><td>%s</td>" % (group, )
+		table += u"<td>"
+  		grptable = u"<table width=\"100%\"><tr>"
+		k=0
+		for p in grpvalue[3]:
+			grptable += "<td width=12.5%><a href=\""+userbase + p +"\">" + unicode(p) + "</a></td>"
+			if k%8 == 7:
+				grptable += "</tr><tr>"
+			k= k+1
+		grptable += "</tr></table>"
+		table += grptable
+		comment = "asdf"
+		table += u"</td><td>%s</td></tr>" % (comment, )
+		print_ui(showgrouppage % (group,table,))
+	else:
+		print_ui(errorpage % ("No group specified",))
+
+def mainpage_ui(form):
+	table = u"<table border=1><tr><td>Группа</td><td>Пользователи</td><td>Комментарий к группе</td></tr>"
+	for i in grp.getgrall():
+	  	if (i[2] > 1000) and (i[2] <=64000):
+			table += "<tr><td><a href=./?page=showgroup&group=" + unicode(i[0]) + ">" + unicode(i[0]) + "</a></td>"
+			k=len(i[3])
+			table += u"<td>%d</td>" % k
+#	  		table += u"<table width=\"100%\"><tr>"
+#			k=0
+#			for p in i[3]:
+#				table += "<td width=12.5%><a href=\""+userbase + p +"\">" + unicode(p) + "</a></td>"
+#				if k%8 == 7:
+#					table += "</tr><tr>"
+#				k= k+1
+#			table += "</tr></table>"
+			table += "<td></td></tr>"
+	table+="</table>"
+	print_ui(mainpage % (table,))
+	pass
+
 form = cgi.FieldStorage()
+
+functions = { "listadd": listadd_ui, "showgroup": showgroup_ui, "mainpage": mainpage_ui, }
+
+if "page" in form:
+	header_html()
+	functions[form["page"].value](form)
+	exit(0)
 
 if "listadd" in form:
 	header_html()
 	print_ui(queuepage)
 	exit(0)
 
-if "searchkey" in form or 1:
-	header_html()
-	table = u"<table border=1><tr><td>Группа</td><td>Пользователи</td><td>Комментарий к группе</td></tr>"
-	for i in grp.getgrall():
-	  	if (i[2] > 1000) and (i[2] <=64000):
-			table += "<tr><td>" + unicode(i[0]) + "</td><td>"
-	  		table += u"<table width=\"100%\"><tr>"
-			k=0
-			for p in i[3]:
-				table += "<td width=12.5%><a href=\""+userbase + p +"\">" + unicode(p) + "</a></td>"
-				if k%8 == 7:
-					table += "</tr><tr>"
-				k= k+1
-			table += "</tr></table>"
-			table += "</td></tr>"
-	table+="</table>"
-	print_ui(mainpage % (table,))
-	exit(0)
-
 header_html()
-print_ui(mainpage % "")
+mainpage_ui(form)
 exit(0)
