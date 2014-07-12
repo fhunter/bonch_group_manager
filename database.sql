584235f4d23811edbf5b46b9e66c9e73d5bdc8e1
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2014-07-12 17:52:14 +0400

    Added comments table

--- database.sql
+++ database.sql
@@ -1,4 +1,5 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 CREATE TABLE queue (id integer primary key autoincrement, groupame text not null, usernumber integer not null default 40, date datetime not null default current_timestamp, done boolean not null default 'false', addedby text not null);
+CREATE TABLE comments(id integer primary key autoincrement, groupname text not null unique, comment text);
 COMMIT;
